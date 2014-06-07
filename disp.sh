07e22ab79c122123c6513732f7729a839d2da3f1
Kingspp <kingspprathyush@gmail.com>
2014-06-07 10:02:17 -0600

    Display Pi v1.0 Added Overview for Now

--- disp.sh
+++ disp.sh
@@ -42,19 +42,51 @@ echo ""
 echo ""
 echo "Thank You"
 echo "By Kingspp"
-echo " Reboot the system to apply changes?? [y/n]"
-read ch
-if [ "$ch" == 'y' ]; then
-reboot
-else
 exit
-fi
 }
 
 function overview()
 {
-echo "Pi Temperature:"
-/opt/vc/bin/vcgencmd measure_temp
+clear
+top
+echo ""
+echo "Version: "
+echo "-------------"
+/opt/vc/bin/vcgencmd version
+echo ""
+
+echo "Device Specifics: "
+echo "--------------------------"
+pitemp=$(/opt/vc/bin/vcgencmd measure_temp )
+arm_f=$(/opt/vc/bin/vcgencmd measure_clock arm)
+core_f=$(/opt/vc/bin/vcgencmd measure_clock core)
+core_v=$(/opt/vc/bin/vcgencmd measure_volts core)
+arm_m=$(/opt/vc/bin/vcgencmd get_mem arm)
+gpu_m=$(/opt/vc/bin/vcgencmd get_mem gpu)
+echo "Pi $pitemp"
+echo "ARM $arm_f Hz"
+echo "Core $core_f Hz"
+echo "Core $core_v "
+echo "ARM Memory: $arm_m"
+echo "GPU Memory: $gpu_m"
+gv=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
+echo "Governor in use: $gv"
+echo ""
+
+echo "Config.txt Options:"
+echo "---------------------------"
+/opt/vc/bin/vcgencmd get_config int
+echo ""
+
+echo "Codecs Enabled:"
+echo "--------------------------"
+for codec in H264 MPG2 WVC1 MPG4 MJPG WMV9 ; do 
+     echo -e "$codec:\t$(/opt/vc/bin/vcgencmd codec_enabled $codec)" ; 
+ done 
+ echo ""
+ 
+thank
+exit
 }
 
 function mountm()
@@ -78,19 +110,17 @@ echo ""
 sleep 1
 }
 
-
 function ui()
 {
 clear
 top
 echo "########################################################################"
 echo "1.Overview"
-echo "2.Configuration"
-echo "3.Overclocking Settings"
-echo "4..Temperature"
-echo "5.Mounting Options"
+echo "2.Configuration **"
+echo "3.Overclocking Settings **"
+echo "4..Temperature **"
+echo "5.Mounting Options **"
 echo "########################################################################"
-sleep 1
 echo "Choose among the following [1-5]"
 echo ""
 read opt
@@ -106,13 +136,6 @@ case $opt in
 5) mountm
 ;;
 
-
-
-
-
-
-
-
 esac
 }
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -123,3 +146,4 @@ esac
 ## To check if its running as Root
 checkr
 ui
+overview
