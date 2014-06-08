d720be960f8aef0388970bec634fe6ae47ee5c91
kingspp <kingspprathyush@.com>
2014-06-08 03:20:53 -0600

    Updated Display Pi to 1.1

--- disp.sh
+++ disp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Display Pi v1.0
+# Display Pi v1.1
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
 #the Free Software Foundation, either version 3 of the License, or
