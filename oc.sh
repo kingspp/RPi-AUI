530c1456f12874a10e51a5015c86727e8922a23c
kingspp <kingspprathyush@.com>
2014-06-08 10:07:51 -0600

    Cleaned the Script

--- oc.sh
+++ oc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Overclocking PI v1.2
+# Overclocking Pi v1.2
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
 #the Free Software Foundation, either version 3 of the License, or
