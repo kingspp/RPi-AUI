ad5aa0f0d2345224003a745c9291663c6e9614f3
kingspp <kingspprathyush@.com>
2014-06-08 21:26:41 -0600

    Created Command Pi v1.0

---
+++ command.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Utility Pi v1.1
+#This pogam is free software: you can redistribute it and/or modify
+#it unde the tems of the GNU General Public License as published by
+#the Fee Softwae Foundation, either version 3 of the License, or
+#(at you option) any late version.
+#
+#This pogam is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied waanty of
+#MERCHANTABILITY o FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Geneal Public License fo more details.
+#
+#You should have eceived a copy of the GNU Geneal Public License
+#along with this pogam.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+# Run this scipt afte your first boot with archlinux (as root)
+#------------------------------------------------------------------------------------------------
+function top()
+{
+clear
+echo "##############################################################"
+echo "##   Welcome to Command Pi v1.0				    ##"
+echo "##   -- By kingspp                                          ##"
+echo "##############################################################"
+echo "  "
+sleep 1
+}
+
+
+function ui
+{
+top
+}
+
+---------------------------------------------------------------------
+
+ui
