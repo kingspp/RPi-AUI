1f8437296ab9e00602af012d182a480b683b4873
Prathyush <kingspprathyush@gmail.com>
2014-10-10 06:54:48 +0530

    Refactored the code

--- JavaIO/time.java
+++ JavaIO/time.java
@@ -10,43 +10,42 @@ import com.pi4j.io.gpio.RaspiPin;
 
 public class time 
 {
-
-	    static final GpioController gpio = GpioFactory.getInstance();
+ 	//Create a GPIO Instance
+	static final GpioController gpio = GpioFactory.getInstance();
         
         // Turn on the control pins of the SSD
         static final GpioPinDigitalOutput cpin1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
-		static final GpioPinDigitalOutput cpin2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "MyLED", PinState.HIGH);
-		static final GpioPinDigitalOutput cpin3 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "MyLED", PinState.HIGH);
-		static final GpioPinDigitalOutput cpin4 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "MyLED", PinState.HIGH);
+        static final GpioPinDigitalOutput cpin2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "MyLED", PinState.HIGH);
+	static final GpioPinDigitalOutput cpin3 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "MyLED", PinState.HIGH);
+	static final GpioPinDigitalOutput cpin4 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "MyLED", PinState.HIGH);
 		
-		// Tun on the LED
-		static final GpioPinDigitalOutput a = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "MyLED", PinState.HIGH);
-		static final GpioPinDigitalOutput b = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06, "MyLED", PinState.HIGH);
-		static final GpioPinDigitalOutput c = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "MyLED", PinState.HIGH);
-		static final GpioPinDigitalOutput d = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_08, "MyLED", PinState.HIGH);
-		static final GpioPinDigitalOutput e = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_09, "MyLED", PinState.HIGH);
-		static final GpioPinDigitalOutput f = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_10, "MyLED", PinState.HIGH);
-		static final GpioPinDigitalOutput g = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_11, "MyLED", PinState.HIGH);
-		static final GpioPinDigitalOutput h = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_12, "MyLED", PinState.HIGH);
+	// Tun on the each pin of SSD
+	static final GpioPinDigitalOutput a = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "MyLED", PinState.HIGH);
+	static final GpioPinDigitalOutput b = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06, "MyLED", PinState.HIGH);
+	static final GpioPinDigitalOutput c = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "MyLED", PinState.HIGH);
+	static final GpioPinDigitalOutput d = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_08, "MyLED", PinState.HIGH);
+	static final GpioPinDigitalOutput e = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_09, "MyLED", PinState.HIGH);
+	static final GpioPinDigitalOutput f = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_10, "MyLED", PinState.HIGH);
+	static final GpioPinDigitalOutput g = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_11, "MyLED", PinState.HIGH);
+	static final GpioPinDigitalOutput h = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_12, "MyLED", PinState.HIGH);
 
 	public static void main(String args[]) throws InterruptedException
 	{
 		DateFormat hour = new SimpleDateFormat("HH");
 		DateFormat min = new SimpleDateFormat("mm");
-        Date date = new Date();		
+        	Date date = new Date();		
 		int hr = Integer.parseInt(hour.format(date));
 		int mn = Integer.parseInt(min.format(date));		
 		System.out.println(hr);
 		System.out.println(mn);
-		
 		
 		shutdown();        
 	}
 	
 	static void shutdown () throws InterruptedException
 	{
-	  //Turnoff all the pins
-	    time t=new time();
+	  	//Turnoff all the pins
+	    	time t=new time();
 		Thread.sleep(2000);
 		t.cpin1.low();
 		t.cpin2.low();
@@ -62,6 +61,4 @@ public class time
 		t.h.low();		
 		t.gpio.shutdown();		
 	}
-	
-	
 }
